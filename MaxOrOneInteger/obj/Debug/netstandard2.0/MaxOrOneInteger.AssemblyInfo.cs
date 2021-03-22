25ecdd88be4a29aa9fdf2cff08999f95a8a5c33b
Jhonata Fernandes <jhonata.fernandes@pitang.com>
2021-03-22 19:28:08 -0300

    adicionando versao no pacote

--- MaxOrOneInteger/obj/Debug/netstandard2.0/MaxOrOneInteger.AssemblyInfo.cs
+++ MaxOrOneInteger/obj/Debug/netstandard2.0/MaxOrOneInteger.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jhonatafernandes.com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Function that chooses the maximum number or number one")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaxOrOneInteger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaxOrOneInteger")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
